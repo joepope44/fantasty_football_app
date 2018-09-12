6ef3ece8b509af7f372856dfc95b1a0363eb8f94
josephpope <joepope44@gmail.com>
2018-09-12 11:33:04 -0400

    reduced data for OL and DST madden

--- linear_regression.py
+++ linear_regression.py
@@ -1,17 +1,27 @@
-from nfl_madden.py import df
+# from nfl_madden.py import df
 
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.dummy import DummyRegressor
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
 import seaborn as sns
 sns.set(style="whitegrid")
 
-qbr = df.drop(['Fantasy Points', 'Season', 'Game', 'Players', 'Position_x', 'Position_y', 'Name', 'Team', 'Handedness', 'Week'], axis=1)
+tmp.head()
 
-y = qbr['Passing Yards']
-X = qbr.drop('Passing Yards', axis=1)
+y = tmp['Passing Yards']
+X = tmp.drop(['Passing Yards'], axis=1)
+
+labels = X[['Team', 'Name']]
+
+X.drop(['Team', 'Name', 'Opponent', 'Fantasy Points', 'Total Salary', 'Total Salary_DST'], axis=1, inplace=True)
+
 
 regr = LinearRegression()
 
@@ -32,5 +42,64 @@ names = [X.columns[i] for i in indices]
 
 imp = pd.DataFrame(list(zip(names, importances[indices])))
 
-imp.sort_values(1, ascending=False)
+imp.sort_values(1, ascending=False)[:10]
+
+# LINEAR REGRESSION
+
+X_train, X_test, y_train, y_test = \
+	train_test_split(X, y, test_size=0.2, random_state=44)
+
+# Create a dummy regressor
+dummy_mean = DummyRegressor(strategy='mean')
+
+# "Train" dummy regressor
+dummy_mean.fit(X_train, y_train)
+
+# Get R-squared score
+dummy_mean.score(X_test, y_test) # -0.11 R2 using mean
+
+# vanilla linear regression - R2 is -4!
+model = regr.fit(X_train, y_train)
+
+model.score(X_test, y_test)
+
+# LASSO
+
+# Standarize features
+scaler = StandardScaler()
+X_std = scaler.fit_transform(X_train)
+
+# Create lasso regression with alpha value
+lasso = Lasso(alpha=0.9)
+
+# Fit the linear regression
+lasso_model = lasso.fit(X_std, y_train)
+
+lasso_model.score(X_test, y_test)
+
+# Create ridge regression with an alpha value
+ridge_model = Ridge(alpha=0.3)
+
+# Fit the linear regression
+ridge_model = regr.fit(X_std, y_train)
+
+ridge_model.score(X_test, y_test)
+
+
+model = sm.OLS(y_test, X_test)
+fit = model.fit()
+fit.summary()
+
+data = pd.DataFrame()
+
+data['predict']=fit.predict(X_std)
+data['resid']=y_train - data.predict
+with sns.axes_style('white'):
+    plot=data.plot(kind='scatter',
+                  x='predict', y='resid', figsize=(10,6))
+
+
+X.to_csv('data/regr.csv')
+
+sns.distplot(y)
 
--- nfl_madden.py
+++ nfl_madden.py
@@ -6,22 +6,22 @@ qb_nfl_fields = [
 	'Passes Intercepted', 'Passing Conversions', 'Passing Yards', 'Players',
 	'Rushing Attempts', 'Rushing Conversions', 'Rushing Touchdowns',
 	'Rushing Yards', 'Touchdown Passes', 'Position', 'Week'
-	]
+]
 
 rb_nfl_fields = [
 	'Fantasy Points', 'Game', 'Players', 'Receiving Conversions',
 	'Receiving Yards', 'Receptions', 'Rushing Attempts', 'Rushing Touchdowns',
 	'Rushing Yards', 'Touchdown Receptions', 'Position', 'Week'
-	]
+]
 
 wr_nfl_fields = te_nfl_fields = rb_nfl_fields
 
 dst_nfl_fields = [
-	'Blocked Kicks', 'Fantasy Points', 'Fumble Recoveries', 'Game',
+	'Fantasy Points', 'Game',
 	'Interceptions', 'Passing Yards Allowed', 'Players', 'Points Against',
 	'Rushing Yards Allowed', 'Sacks', 'Safeties', 'Total Yards Allowed',
 	'Touchdowns', 'Position', 'Week', 'Team'
-	]
+]
 
 
 def filter_nfl_data(pos, fields):
@@ -55,21 +55,20 @@ mad_qb_fields = [
 	'Throw Accuracy Mid', 'Throw Accuracy Short', 'Throw Power',
 	'Throw on the Run', 'Total Salary', 'Toughness', 'Weight', 'Years Pro',
 	'Season'
-	]
+]
 
 mad_dst_fields = [
 	'Age', 'Acceleration', 'Agility', 'Awareness', 'Block Shedding',
-	'Finesse Moves', 'Height', 'Hit Power', 'Impact Blocking', 'Injury',
-	'Jumping', 'Man Coverage', 'Name', 'Overall Rating', 'Play Recognition',
+	'Finesse Moves', 'Height', 'Hit Power', 'Impact Blocking',
+	'Man Coverage', 'Name', 'Overall Rating', 'Play Recognition',
 	'Position', 'Power Moves', 'Speed', 'Spin Move', 'Stamina', 'Strength',
-	'Tackle', 'Team', 'Total Salary', 'Toughness', 'Weight', 'Years Pro',
-	'Zone Coverage', 'Season'
-	]
+	'Tackle', 'Team', 'Weight', 'Years Pro', 'Zone Coverage', 'Season'
+]
 
 mad_offline_fields = [
-	'Age', 'Acceleration', 'Agility', 'Awareness', 'Height', 'Injury', 'Name',
-	'Overall Rating', 'Pass Block', 'Position', 'Speed', 'Stamina', 'Strength',
-	'Team', 'Total Salary', 'Toughness', 'Weight', 'Years Pro', 'Season'
+	'Height', 'Overall Rating', 'Pass Block',
+	'Position', 'Stamina', 'Strength', 'Team',
+	'Weight', 'Years Pro', 'Season'
 ]
 
 mad_rb_fields = [
@@ -94,8 +93,7 @@ def filter_madden_data(pos, fields, season):
 	"""
 	madden = pd.read_csv('data/madden3.csv')
 
-
-	#handle DST and OL positions
+	# handle DST and OL positions
 	DST_list = ['LOLB', 'MLB', 'RE', 'LE', 'CB', 'FS', 'SS', 'ROLB', 'DT']
 	OL_list = ['LT', 'LG', 'C', 'RG', 'RT']
 
@@ -119,6 +117,39 @@ mad_wr = filter_madden_data('WR', mad_wr_fields, '2017-2018')
 mad_dst = filter_madden_data('DST', mad_dst_fields, '2017-2018')
 mad_ol = filter_madden_data('OL', mad_offline_fields, '2017-2018')
 
+# average top 7 offensive line players per team
+# mad_ol = mad_ol.groupby('Team', as_index=False)\
+# 	.sort_values(by='Overall Rating', ascending=False)\
+# 	.head(5)\
+# 	.mean()
+
+# mad_ol.groupby('Team', as_index=False).agg
+
+# take the max rating for each offensive line position, ignore all other ratings
+mad_ol_agg = mad_ol.pivot_table(values='Overall Rating',
+								index='Team',
+								columns='Position',
+								aggfunc=np.max).reset_index()
+
+# create average of the OL
+mad_ol_agg['OL_AVG'] = mad_ol_agg.mean(axis=1)
+
+# take max rating for each defensive player
+mad_dst_agg = mad_dst.pivot_table(values='Overall Rating',
+					index='Team',
+					columns='Position',
+					aggfunc=np.max).reset_index()
+
+mad_dst_agg['DST_AVG'] = mad_dst_agg.mean(axis=1).round(1)
+
+mad_dst_agg['Backfield_AVG'] = mad_dst_agg[['CB', 'FS', 'SS']].mean(axis=1).round(1)
+
+mad_dst_agg['Linebackers AVG'] = mad_dst_agg[['LOLB', 'MLB', 'ROLB']].mean(axis=1).round(1)
+
+mad_dst_agg['DL_AVG'] = mad_dst_agg[['DT', 'LE', 'RE']].mean(axis=1).round(1)
+
+
+# mad_ol.columns = [str(col) + '_OL' for col in mad_ol.columns]
 
 # need to figure out how to handle DST and OL, position doesn't match up with
 # NFL data
@@ -144,6 +175,14 @@ rb_data = merge_nfl_madden(nfl_rb, mad_rb)
 wr_data = merge_nfl_madden(nfl_wr, mad_wr)
 te_data = merge_nfl_madden(nfl_te, mad_te)
 
+# aggregate defensive stats for top 14 players by Overall Rating
+dst_data.columns = [str(col) + '_DST' for col in dst_data.columns]
+dst_agg_data = (dst_data.groupby('Team_DST')
+				.mean()
+				.sort_values(by='Overall Rating_DST', ascending=False)
+				.head(14)
+				.round(3))
+
 # dst_data.to_csv('data/dst_data_clean.csv')
 
 # preprocess before regression analysis. look at player, their opponents DST
@@ -154,46 +193,47 @@ sched['Week'] = sched['Week'].astype(float)
 
 
 def regr_preprocess(pos_data):
-
 	global dst_data
 	global mad_ol
 	global sched
 
 	# merge all home and away game schedule data with positional NFL and Madden data
-	away = pd.merge(pos_data, sched, left_on=['Week','Team'], right_on=['Week','Away'])
-	home = pd.merge(pos_data, sched, left_on=['Week','Team'], right_on=['Week','Home'])
+	away = pd.merge(pos_data, sched, left_on=['Week', 'Team'], right_on=['Week', 'Away'])
+	home = pd.merge(pos_data, sched, left_on=['Week', 'Team'], right_on=['Week', 'Home'])
 	df = pd.concat([away, home])
 
 	df.drop([
-		'Game', 'Position_x', 'Position_y', 'Handedness', 'Players', 'Season', 'Day',\
-		'Date', 'Time', 'Winner/tie', 'At', 'Loser/tie', 'Away Team', 'Home Team ',\
-		'PtsW', 'PtsL', 'TOW', 'YdsL','TOL'\
+		'Game', 'Position_x', 'Position_y', 'Handedness', 'Players', 'Season', 'Day',
+		'Date', 'Time', 'Winner/tie', 'At', 'Loser/tie', 'Away Team', 'Home Team ',
+		'PtsW', 'PtsL', 'TOW', 'YdsL', 'TOL'
 	], axis=1, inplace=True)
 
 	# input one if players team is home team, else zero for away team.
 	df['isHomeTeam'] = df.apply(lambda x: x['Team'] in x['Home'], axis=1).astype(int)
 	# identify opponent
-	df['Opponent'] = df.apply(lambda x: x['Away'] if x['isHomeTeam'] == 1 else x['Home'],
-							  axis=1)
+	df['Opponent'] = df.apply(lambda x: x['Away'] if x['isHomeTeam'] == 1 else x['Home'], axis=1)
 
 	# now drop away and home fields.
 	df.drop(['Home', 'Away'], axis=1, inplace=True)
 
 	# merge offensive line stats
-	# consider dropping off most of these stats
-	# df = pd.merge(df, mad_ol, on='Team', suffixes='_OL')
+	df = pd.merge(df, mad_ol, how='left', on='Team')
 
 	# merge defensive stats
+	df = pd.merge(df, dst_agg_data, how='left', left_on='Opponent', right_on='Team_DST')
 
 	return df
 
 
 tmp = regr_preprocess(qb_data)
+tmp.shape
 
-
-
-
-
+tmp.to_csv('data/check.csv')
+#
+# def test_data(player, position, opponent):
+# 	tmp = regr_preprocess(qb_data)
+#
+# 	tmp['Name'] == player
 
 
 # df.to_csv('data/qb_clean.csv')
